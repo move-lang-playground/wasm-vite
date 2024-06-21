ea8fb7094ac39d82a85a38a6e3d9d9433bf164ab
WGB5445 <919603023@qq.com>
2024-06-21 13:52:30 +0800

    fix package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { render } from 'preact'
 import { App } from './app.tsx'
 import './index.css'
-import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { AptosWalletAdapterProvider,  } from "@aptos-labs/wallet-adapter-react";
+import {Network} from "@aptos-labs/ts-sdk"
 render(<AptosWalletAdapterProvider
-    plugins={[]}
+    plugins={[
+    ]}
     autoConnect={true}
+    dappConfig={{network: Network.DEVNET}}
     onError={(error: any) => {
       console.log("error", error);
     }}
